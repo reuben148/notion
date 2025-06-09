17cee56d72889ffa96b272d648b11a4d78641a12
reuben148 <reubenluka555@gmail.com>
2025-06-09 17:48:49 +0100

    fixed issues

--- src/Contents/Blog.jsx
+++ src/Contents/Blog.jsx
@@ -2,19 +2,17 @@ import React from "react";
 
 function Blog() {
   return (
-    <div>
-      <div className="flex justify-center items-center space-x-4">
-        <img
-          src="practice.jpg"
-          alt=""
-          className="w-[250px] h-[250px] object-cover"
-        />
-        <img
-          src="practice2.jpg"
-          alt=""
-          className="w-[250px] h-[250px] object-cover"
-        />
-      </div>
+    <div className="flex justify-center items-center flex-wrap gap-4">
+      <img
+        src="practice.jpg"
+        alt=""
+        className="w-[250px] h-[250px] object-cover sm:w-[200px] sm:h-[200px] md:w-[250px] md:h-[250px]"
+      />
+      <img
+        src="practice2.jpg"
+        alt=""
+        className="w-[250px] h-[250px] object-cover sm:w-[200px] sm:h-[200px] md:w-[250px] md:h-[250px]"
+      />
     </div>
   );
 }
--- src/Contents/Header.jsx
+++ src/Contents/Header.jsx
@@ -4,14 +4,15 @@ function Header() {
   return (
     <div>
       <div>
-        <p className="px-6 py-6 text-center text-6xl">
+        <p className="px-6 py-6 text-center text-4xl sm:text-5xl md:text-6xl">
           <span className="text-yellow-500">Your💡ideas</span>,
           <span className="text-blue-500">📚 docs</span>,
           <br />& <span className="text-green-500">🎯 projects</span>. Together.
         </p>
-        <p className="text-center text-2xl font-serif">
+        <p className="text-center text-xl sm:text-2xl font-serif">
           Station is the connected workspace where
-          <br></br>better, faster work happens.
+          <br />
+          better, faster work happens.
         </p>
       </div>
     </div>
