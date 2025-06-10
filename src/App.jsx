14d66c595926ebe2d6a436258d20ba6e67c69a6d
reuben148 <reubenluka555@gmail.com>
2025-06-10 11:47:25 +0100

    pushing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./Pages/Signup";
 import { Header, Navbar, Blog } from "./Contents";
 import { Button, Footer } from "./Containers";
 
